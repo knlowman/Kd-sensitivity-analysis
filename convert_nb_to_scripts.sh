fac6e3c3a8a60d1bf6d1cbd55772ebe1b2c02e5d
Kiera Lowman <knlowman@princeton.edu>
2025-01-24 17:46:46 -0500

    updated paths

--- convert_nb_to_scripts.sh
+++ convert_nb_to_scripts.sh
@@ -1,6 +1,6 @@
 # script to convert certain notebooks to .py files
 
-jupyter nbconvert --to script --no-prompt read_and_calculate.ipynb
-jupyter nbconvert --to script --no-prompt plotting_functions.ipynb
-jupyter nbconvert --to script --no-prompt read_mem1_functions.ipynb
-jupyter nbconvert --to script --no-prompt custom_moc_functions.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/read_and_calculate.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/plotting_functions.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/read_mem1_functions.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/custom_moc_functions.ipynb
