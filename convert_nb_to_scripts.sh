9f137d0b7b0a4de3ae2b4e75ec414ab5a4761d1e
Kiera Lowman <knlowman@princeton.edu>
2026-04-14 17:21:12 -0400

    Various edits

--- convert_nb_to_scripts.sh
+++ convert_nb_to_scripts.sh
@@ -4,3 +4,8 @@ jupyter nbconvert --to script --no-prompt notebooks/read_and_calculate.ipynb
 jupyter nbconvert --to script --no-prompt notebooks/plotting_functions.ipynb
 jupyter nbconvert --to script --no-prompt notebooks/read_mem1_functions.ipynb
 jupyter nbconvert --to script --no-prompt notebooks/custom_moc_functions.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/compute_ensemble_means.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/time_series_plotting.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/sea_level_plot_functions.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/sea_ice_plotting.ipynb
+jupyter nbconvert --to script --no-prompt notebooks/AM2_plot_functions.ipynb
